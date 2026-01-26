ce0ef809eb8f570fe9c81b92e16a94653c9c12b9
kishpatel0446 <kishpatel7069@gmail.com>
2026-01-26 14:12:02 +0530

    Updated Reverse Number File With Exception Handling

--- rev_num_loop.py
+++ rev_num_loop.py
@@ -1,17 +1,25 @@
-n = int(input("Enter Number:"))
+#Input from user with exception handling
+try:
+    n = int(input("Enter Number:"))
+except ValueError:
+    print("Invalid Input. There is no other datatype allowed except integer")
+else:
+    rev = 0
+    is_neg = False
 
-rev = 0
-is_neg = False
+    # Handle Negative
+    if n < 0:
+        is_neg = True
+        n = -n
+    # Reverse Number using While loop
+    while n > 0:
+        d = n % 10
+        rev = rev * 10 + d
+        n = n // 10
 
-if n < 0:
-    is_neg = True
-    n = -n
+    # Put negative symball if number is negative
+    if is_neg:
+        rev = -rev
 
-while n > 0:
-    d = n % 10
-    rev = rev * 10 + d
-    n = n // 10
-
-if is_neg:
-    rev = -rev
-print(rev)
+    #Print Output
+    print(rev)
