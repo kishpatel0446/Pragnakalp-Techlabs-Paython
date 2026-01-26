cfdcd2492b22e1eddee9736c744ccd113f7b3d6d
kishpatel0446 <kishpatel7069@gmail.com>
2026-01-26 14:15:28 +0530

    Updated Sum Even Number File With Exception Handling

--- sum_ev_loops.py
+++ sum_ev_loops.py
@@ -1,15 +1,21 @@
-n = int(input("Enter a Number: "))
+#Take input from user with exception handling
 
-#Handle of Edge case n<1
+try:
+    n = int(input("Enter a Number: "))
+except:
+    print("Invalid Input. No other datatype allowed except Integer")
 
-if n < 1:
-    print(0)
 else:
-    total = 0
-    i = 1
+    # Handle of Edge case n<1
 
-    while i <= n:
-        if i % 2 == 0:
-            total += i
-        i += 1
-    print(total)
+    if n < 1:
+        print(0)
+    else:
+        total = 0
+        i = 1
+
+        while i <= n:
+            if i % 2 == 0:
+                total += i
+            i += 1
+        print(total)
