15c2aee7e8e27b84af1a5beabeb49452e70c03ca
kishpatel0446 <kishpatel7069@gmail.com>
2026-01-26 13:59:03 +0530

    Added Reverse Number File

---
+++ rev_num_loop.py
@@ -0,0 +1,17 @@
+# n = int(input("Enter Number:"))
+#
+# rev = 0
+# is_neg = False
+#
+# if n < 0:
+#     is_neg = True
+#     n = -n
+#
+# while n > 0:
+#     d = n % 10
+#     rev = rev * 10 + d
+#     n = n // 10
+#
+# if is_neg:
+#     rev = -rev
+print(100//10)
