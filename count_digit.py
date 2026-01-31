80d8caae9b1a1b26a05efb0a4bd22557d575894f
kishpatel0446 <kishpatel7069@gmail.com>
2026-01-31 16:26:54 +0530

    Added File for Count Digit

---
+++ count_digit.py
@@ -0,0 +1,33 @@
+def countDigit(number,digit):
+
+    count = 0
+    # For Digit range 0 to 9
+    if digit < 0 or digit > 9:
+        print("Invalid Digit. Digit must be in range from 0 to 9")
+    else:
+        # for negative number
+        if number < 0:
+            number = -number
+
+        temp = number
+
+        while temp > 0:
+            d = temp % 10
+            if d == digit:
+                count += 1
+            temp = temp // 10
+        print(count)
+
+# Handle Invalid Input
+try:
+    num = int(input("Enter Number: "))
+except ValueError:
+    print("Invalid Number!")
+    exit()
+try:
+    dig = int(input("Enter Digit You Want to Count: "))
+except ValueError:
+    print("Invalid Digit!")
+    exit()
+
+countDigit(num,dig)
