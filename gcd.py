508111c18cc5b2f7d74e5534d321c2ba866d635b
kishpatel0446 <kishpatel7069@gmail.com>
2026-01-31 16:32:29 +0530

    Added File For Find GCD

--- gcd.py
+++ gcd.py
@@ -1,4 +1,12 @@
-def gcd(num1,num2):
+def find_gcd(num1,num2):
+
+    if num1 == 0 and num2 == 0:
+        return "GDC IS UNDEFINED FOR 0"
+
+    if num1 == 0:
+        return num2
+    if num2 == 0:
+        return num1
 
     while num2 != 0:
         rem = num1 % num2
@@ -10,7 +18,7 @@ def gcd(num1,num2):
 try:
     num1 = int(input("Enter 1st Number: "))
     num2 = int(input("Enter 2nd Number: "))
-    print(gcd(num1,num2))
+    print(find_gcd(num1,num2))
 
 except ValueError:
     print("Enter Valid Numbers")
