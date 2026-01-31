6a7c87d4d08f399d64c1cdd6a7cedfdc30281d4f
kishpatel0446 <kishpatel7069@gmail.com>
2026-01-31 18:22:24 +0530

    Added Vowel & Consonants Count from String File

--- vowels_consonants.py
+++ vowels_consonants.py
@@ -3,13 +3,16 @@ def countVowelsAndConsonants(str):
     vowels = 0
     consonants = 0
 
+    #Check for Empty String
     if str == "":
         print("Vowels:",vowels,", Consonants:",consonants)
         return
 
+    #Check Only for Characters in string not numbers, if numbers found then they will ignore from count
     for char in str:
-        if('A' <= char <= 'Z') or ('a' <= char <= 'z'):
+        if ('A' <= char <= 'Z') or ('a' <= char <= 'z'):
 
+            #Check for UC & LC for Vowels
             if (char == 'a' or char == 'A' or char == 'e' or char == 'E' or char == 'i' or
                     char == 'I' or char == 'o' or char == 'O' or char == 'u' or char == 'U'):
                 vowels += 1
