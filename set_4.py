d81ed4a77e9b4958f4726ea1cc38d4a61473b656
kishpatel0446 <kishpatel7069@gmail.com>
2026-03-07 15:10:52 +0530

    Added Set Question 4

---
+++ set_4.py
@@ -0,0 +1,24 @@
+s1_input = input("Enter first set: ")
+s2_input = input("Enter second set: ")
+if s1_input == "":
+    s1 = set()
+else:
+    s1 = eval(s1_input)
+
+if s2_input == "":
+    s2 = set()
+else:
+    s2 = eval(s2_input)
+
+subset = True
+
+for x in s1:
+    found = False
+    for y in s2:
+        if x == y:
+            found = True
+            break
+
+    if found == False:
+        subset = False
+print(subset)
