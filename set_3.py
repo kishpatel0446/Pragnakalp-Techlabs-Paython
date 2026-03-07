801d3bb8d32586b6dd840f7ad3d02102858aa959
kishpatel0446 <kishpatel7069@gmail.com>
2026-03-07 13:56:48 +0530

    Added Set Question 3

---
+++ set_3.py
@@ -0,0 +1,29 @@
+s1_input = input("Enter first set: ")
+s2_input = input("Enter second set: ")
+if s1_input == "":
+    s1 = set()
+else:
+    s1 = eval(s1_input)
+
+if s2_input == "":
+    s2 = set()
+else:
+    s2 = eval(s2_input)
+print("{", end="")
+
+first = True
+
+for x in s1:
+    found = False
+    for y in s2:
+        if x == y:
+            found = True
+            break
+
+    if found == False:
+        if first == False:
+            print(",", end="")
+        print(x, end="")
+        first = False
+
+print("}")
